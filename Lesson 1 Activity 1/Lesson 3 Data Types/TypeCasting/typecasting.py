3ed8bf6414ae80efe2b4c24806dac7a3e2f9073e
therohankun-collab <therohankun@gmail.com>
2026-04-04 19:56:13 +0530

    Typecasting

---
+++ Lesson 1 Activity 1/Lesson 3 Data Types/TypeCasting/typecasting.py
@@ -0,0 +1,26 @@
+Name = "Penguin"
+Age = 15
+Is_Student = True
+Weight = 38.5
+
+print ("Name:\n", Name)
+print ("Data Type of name is:", type(Name))
+
+print ("Age:", Age)
+print ("Data Type of age is:", type(Age))
+
+
+print ("Is Student:", Is_Student)
+print ("Data Type of Is Student is:", type(Is_Student))
+
+
+print ("Weight:", Weight)
+print ("Data Type of Weight is:", type(Weight))
+
+Age = str(Age)
+print (Age)
+print ("Data type of age is:", type(Age))
+
+Weight = int(Weight)
+print ("Weight")
+print ("Data type of weight is:", type(Weight))
