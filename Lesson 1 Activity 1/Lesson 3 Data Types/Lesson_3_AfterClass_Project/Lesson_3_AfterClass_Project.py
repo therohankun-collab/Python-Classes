0b87ce1744962604ff3b505138e923e8d7f9c6ae
therohankun-collab <therohankun@gmail.com>
2026-04-05 15:05:47 +0530

    L3_A-C_Project

---
+++ Lesson 1 Activity 1/Lesson 3 Data Types/Lesson_3_AfterClass_Project/Lesson_3_AfterClass_Project.py
@@ -0,0 +1,14 @@
+print ("Lower Case To Upper Case Transformer") 
+print ("In Raj's Case, the Result was:")
+word = "congratulations"
+upper_word = word.upper()
+
+print(upper_word)
+
+# Input any word or sentence here
+word = input("Enter the word to transform: ")
+
+transformed_word = word.upper()
+
+print("Result:", transformed_word)
+#I  just like to make things better
