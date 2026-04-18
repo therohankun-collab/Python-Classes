c2a21b9d23f95511a5dcdbb735c60ce1d2263b92
therohankun-collab <therohankun@gmail.com>
2026-04-18 20:02:49 +0530

    activity4

---
+++ Lesson 1 Activity 1/Lesson 3 Data Types/Lesson_3_AfterClass_Project/Python Operators-1/Python Operators AO/PO_AC_Project/Conditional_Statements/Activity4.py
@@ -0,0 +1,8 @@
+number = (input("Enter Number To Check"))
+print("number to be checked:", number)
+
+if number%2==0:
+    print("This is an even number")
+
+else:
+    print("this is an odd number")
