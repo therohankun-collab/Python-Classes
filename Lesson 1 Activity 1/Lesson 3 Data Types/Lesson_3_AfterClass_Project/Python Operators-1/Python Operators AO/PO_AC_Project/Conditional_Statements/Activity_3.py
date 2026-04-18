2176b2de71a603865e2b7c140cb9d41e69d3bae4
therohankun-collab <therohankun@gmail.com>
2026-04-18 19:53:46 +0530

    activity 3

---
+++ Lesson 1 Activity 1/Lesson 3 Data Types/Lesson_3_AfterClass_Project/Python Operators-1/Python Operators AO/PO_AC_Project/Conditional_Statements/Activity_3.py
@@ -0,0 +1,8 @@
+i = int(input("Enter A Number :"))
+if (i<15):
+    print("i is smaller than 15")
+    print("i'm in if block")
+
+else:
+    print("i is greater than 15")
+    print("i'm in else block")
