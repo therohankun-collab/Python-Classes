b9e8ec382d5ab8a80e4d0c28e725cb04488bb8eb
therohankun-collab <therohankun@gmail.com>
2026-04-18 19:36:06 +0530

    Conditional_Statements

---
+++ Lesson 1 Activity 1/Lesson 3 Data Types/Lesson_3_AfterClass_Project/Python Operators-1/Python Operators AO/PO_AC_Project/Conditional_Statements/Activity_1.py
@@ -0,0 +1,10 @@
+num = 4
+if num > 0:
+    print(num, "Is a positive number")
+
+    num1 = -1
+    if num1 > 0:
+        print(num1, "Is a negative number")
+
+    else: 
+        print (num1, "is a negative number")
