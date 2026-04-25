4e674794f252f0fcb320fc78eea40aa4d9d77c45
therohankun-collab <therohankun@gmail.com>
2026-04-25 19:40:36 +0530

    PO-II AND_OR

---
+++ AfterClass_Projects/Python Operators -II/Activity 1 AND_OR.py
@@ -0,0 +1,24 @@
+a = 12
+b = 10
+c = 0
+
+if a and b and c:
+    print("All the numbers have boolean values as 'True' And It Is If Statement")
+else:
+    print("At least on number has boolean value as 'False' And It Is Else Statement")
+
+
+a = 12
+b = -10
+c = 0
+
+if a >0 or b>0:
+    print("Either of the number is greater than zero")
+else:
+    print("None of the numbers is greater than zero")
+
+if b >0 or c>0:
+    print("Either of the number is greater than zero")
+
+else:
+    print("None of the numbers is greater than zero")
