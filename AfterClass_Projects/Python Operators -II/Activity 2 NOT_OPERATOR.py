48d3c14be01f87f95db6ad02b9c853d21f89cc6b
therohankun-collab <therohankun@gmail.com>
2026-04-25 19:53:21 +0530

    PO-II NOT OPERATOR

---
+++ AfterClass_Projects/Python Operators -II/Activity 2 NOT_OPERATOR.py
@@ -0,0 +1,24 @@
+a = 10
+b = 12
+c = 12
+
+print(not(a==b))
+
+print(not(b==c))
+
+a="python"
+b="coding"
+
+if not (a==b):
+    print(a, "and", b, "are different")
+
+    a = 4
+    b = 5
+     
+if not (a==1) == (b==5):
+    print("Hello")
+
+a = int(input("Enter A Number:"))
+
+if not (a % 2 == 0):
+    print(a, "is an odd number.")
