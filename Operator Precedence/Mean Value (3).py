0f72a6fecef205816a589e1b99a1991785d50f2d
therohankun-collab <therohankun@gmail.com>
2026-05-16 19:53:37 +0530

    OP-III

---
+++ Operator Precedence/Mean Value (3).py
@@ -0,0 +1,13 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 40
+#sum of 40 numbers
+sum = mean1*total_number
+print("The Sum Of 40 Numbers: ", sum)
+
+num2 = sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number)-(correct_number)): ", num2)
+
+mean2 = num2/total_number
+print(mean2)
