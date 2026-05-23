f252fbbd8058301b1cbe2d4dc8082316d53d1a9c
therohankun-collab <therohankun@gmail.com>
2026-05-23 19:11:03 +0530

    Swapping_Numbers

---
+++ AfterClass_Projects/23May_2026-Swapping Numbers'.py
@@ -0,0 +1,11 @@
+
+a = input(int)
+b = input(int)
+c = input(int)
+
+print(f"Before swapping: a = {a}, b = {b}, c = {c}")
+
+
+a, b, c = b, c, a
+
+print(f"After swapping:  a = {a}, b = {b}, c = {c}")
