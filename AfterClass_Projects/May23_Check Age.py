27aa9c890eaa63684c336786c642a300f823952f
therohankun-collab <therohankun@gmail.com>
2026-06-08 16:51:20 +0545

    ACP_Check-Age

---
+++ AfterClass_Projects/May23_Check Age.py
@@ -0,0 +1,10 @@
+age = int(input("Enter age: "))
+
+# Nested conditional statements
+if age >= 10:
+    if age <= 20:
+        print("The age is between 10 and 20.")
+    else:
+        print("The age is not between 10 and 20.")
+else:
+    print("The age is not between 10 and 20.")
