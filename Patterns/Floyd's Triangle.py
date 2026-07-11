702c0e02d5a60ed1c60de669670513b533f08346
therohankun-collab <therohankun@gmail.com>
2026-07-11 19:39:03 +0530

    Floyd's Triangle_Pattern

---
+++ Patterns/Floyd's Triangle.py
@@ -0,0 +1,9 @@
+rows = int(input("Enter the number of rows: "))
+number = 1
+
+print("Floyd's Triangle:")
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(number, end=" ")
+        number += 1
+    print()
