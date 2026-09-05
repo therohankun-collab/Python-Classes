1f330f739cc8f6f040b6a3ab57d058846cbc9e10
therohankun-collab <therohankun@gmail.com>
2026-09-05 20:06:31 +0530

    Mathematical_Operations

---
+++ Random & Math Module/Mathematical OPerations.py
@@ -0,0 +1,10 @@
+import math
+print('The Floor & Ceiling Value Of 23.56 Are: ' + str(math.ceil(23.56)) + ', ' + str(math.floor(23.56)))
+
+x = 10
+y = -15
+print('The Value Of x After Copying The Sign From y Is: ' + str(math.copysign(x, y)))
+
+print('The Absolute Value Of -96 & 56 Are: ' + str(math.fabs(-96)) + ', ' + str(math.fabs(56)))
+
+print('The GCD Of 24 & 56 Is: ' + str(math.gcd(24, 56)))
